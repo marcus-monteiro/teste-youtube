cae2fb242dd46395649147ead93e6632d7b64f2a
Marcus Monteiro <contato@marcusmonteiro.com>
2022-04-07 22:52:50 -0300

    primeiro run

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import { Button } from "@material-ui/core";
 
 export default function Home() {
   return (
     <div>
       Clonando youtube
+      <Button color="primary">Hello World</Button>
     </div>
   );
 }
